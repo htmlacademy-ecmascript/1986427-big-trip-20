53ef81358a0fde6d55bf2800a71fd10fc50c1e83
EvgNikitin <evg-nikitin@mail.ru>
2023-06-06 23:01:09 +0300

    module2-task1

--- src/main.js
+++ src/main.js
@@ -3,11 +3,24 @@ import SortView from './view/sort-view.js';
 import TripInfoView from './view/trip-info-view.js';
 import {render, RenderPosition} from './render.js';
 import TripFormPresenter from './presenter/trip-form-presenter.js';
+import RoutePointsModel from './model/route-points-model.js';
+import DestinationsModel from './model/destinations-model.js';
+import OffersModel from './model/offers-model.js';
 
 const tripInfoContainter = document.querySelector('.trip-main');
 const filterContainer = document.querySelector('.trip-controls__filters');
 const sortContainer = document.querySelector('.trip-events');
-const formPresenter = new TripFormPresenter({routePointListContainer: sortContainer});
+
+const routePointsModel = new RoutePointsModel();
+const destinationsModel = new DestinationsModel();
+const offersModel = new OffersModel();
+
+const formPresenter = new TripFormPresenter({
+  routePointListContainer: sortContainer,
+  routePointsModel,
+  destinationsModel,
+  offersModel
+});
 
 render(new TripInfoView, tripInfoContainter, RenderPosition.AFTERBEGIN);
 render(new FilterView(), filterContainer);
--- src/view/route-point-view.js
+++ src/view/route-point-view.js
@@ -1,33 +1,55 @@
 import {createElement} from '../render.js';
+import {humanizeDate, getTimeDiff} from '../utils.js';
 
-function createRoutePointTemplate() {
+const DATE_FORMAT = 'YYYY-MM-DD';
+const EVENT_DATE = 'MMM DD';
+const TIME_FORMAT = 'HH:mm';
+
+
+function createRoutePointTemplate(routePoint, destination, offers) {
+  const {dateFrom, dateTo, type, basePrice, isFavorite} = routePoint;
+
+
+  const dateFormat = humanizeDate(dateFrom, DATE_FORMAT);
+  const eventDate = humanizeDate(dateFrom, EVENT_DATE);
+  const startTime = humanizeDate(dateFrom, TIME_FORMAT);
+  const endTime = humanizeDate(dateTo, TIME_FORMAT);
+  const durationTime = getTimeDiff(dateFrom, dateTo);
+
+  function createOfferTemplate(offersList) {
+    return offersList.map((offer) =>
+      `<li class="event__offer">
+         <span class="event__offer-title">${offer.title}</span>
+         &plus;&euro;&nbsp;
+         <span class="event__offer-price">${offer.price}</span>
+       </li>`).join('');
+  }
+
+  const getFavoriteButton = () => isFavorite ? 'active' : '';
+
   return `<li class="trip-events__item">
   <div class="event">
-    <time class="event__date" datetime="2019-03-18">MAR 18</time>
+  <time class="event__date" datetime="${dateFormat}">${eventDate}</time>
     <div class="event__type">
-      <img class="event__type-icon" width="42" height="42" src="img/icons/check-in.png" alt="Event type icon">
+      <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
     </div>
-    <h3 class="event__title">Check-in Chamonix</h3>
+    <h3 class="event__title">${type} ${destination.name}</h3>
     <div class="event__schedule">
       <p class="event__time">
-        <time class="event__start-time" datetime="2019-03-18T12:25">16:20</time>
+        <time class="event__start-time" datetime="${dateFormat}T${startTime}">${startTime}</time>
         —
-        <time class="event__end-time" datetime="2019-03-18T13:35">17:00</time>
+        <time class="event__end-time" datetime="${dateFormat}T${endTime}">${endTime}</time>
       </p>
-      <p class="event__duration">40M</p>
+      <p class="event__duration">${durationTime}</p>
     </div>
     <p class="event__price">
-      €&nbsp;<span class="event__price-value">600</span>
+      €&nbsp;<span class="event__price-value">${basePrice}</span>
     </p>
     <h4 class="visually-hidden">Offers:</h4>
     <ul class="event__selected-offers">
-      <li class="event__offer">
-        <span class="event__offer-title">Add breakfast</span>
-        +€&nbsp;
-        <span class="event__offer-price">50</span>
-      </li>
+    ${createOfferTemplate(offers)}
     </ul>
-    <button class="event__favorite-btn event__favorite-btn--active" type="button">
+    <button class="event__favorite-btn event__favorite-btn--${getFavoriteButton()}" type="button">
       <span class="visually-hidden">Add to favorite</span>
       <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
         <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"></path>
@@ -41,8 +63,14 @@ function createRoutePointTemplate() {
 }
 
 export default class RoutePointView {
+  constructor({routePoint, destination, offers}){
+    this.routePoint = routePoint;
+    this.destination = destination;
+    this.offers = offers;
+  }
+
   getTemplate() {
-    return createRoutePointTemplate();
+    return createRoutePointTemplate(this.routePoint, this.destination, this.offers);
   }
 
   getElement() {
@@ -57,3 +85,4 @@ export default class RoutePointView {
     this.element = null;
   }
 }
+
