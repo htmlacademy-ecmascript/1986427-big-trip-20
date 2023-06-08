83fa120ee74a621c7481ab77a0ca7975aa986ab2
EvgNikitin <evg-nikitin@mail.ru>
2023-06-08 15:29:12 +0300

    module4-task1

--- src/presenter/trip-form-presenter.js
+++ src/presenter/trip-form-presenter.js
@@ -1,37 +1,65 @@
 import EditFormView from '../view/edit-form-view.js';
 import RoutePointView from '../view/route-point-view.js';
 import RoutePointListView from '../view/route-point-list-view.js';
-import {render} from '../render.js';
+import {render, replace} from '../framework/render.js';
 
 
 export default class TripFormPresenter {
-  routePointListComponent = new RoutePointListView();
+  #routePointsModel = null;
+  #destinationsModel = null;
+  #offersModel = null;
+  #routePointListContainer = null;
+
+  #routePointListComponent = new RoutePointListView();
+
+  #tripRoutePoints = [];
+
 
   constructor({routePointListContainer, routePointsModel, destinationsModel, offersModel}) {
-    this.routePointListContainer = routePointListContainer;
-    this.routePointsModel = routePointsModel;
-    this.destinationsModel = destinationsModel;
-    this.offersModel = offersModel;
+    this.#routePointListContainer = routePointListContainer;
+    this.#routePointsModel = routePointsModel;
+    this.#destinationsModel = destinationsModel;
+    this.#offersModel = offersModel;
   }
 
   init() {
-    this.tripRoutePoints = [...this.routePointsModel.getRoutePoints()];
-    this.destinations = [...this.destinationsModel.getDestinations()];
-
-    render(this.routePointListComponent, this.routePointListContainer);
-
-
-    render(new EditFormView({
-      destination: this.destinationsModel.getById(this.tripRoutePoints[0]),
-      routePoint: this.tripRoutePoints[0],
-      offers: this.offersModel.getByType(this.tripRoutePoints[0]),
-    }), this.routePointListComponent.getElement());
-
-    for (let i = 1; i < this.tripRoutePoints.length; i++) {
-      const destination = this.destinationsModel.getById(this.tripRoutePoints[i]);
-      const offers = this.offersModel.getById(this.tripRoutePoints[i]);
-      render(new RoutePointView({routePoint: this.tripRoutePoints[i], destination: destination, offers: offers}), this.routePointListComponent.getElement());
-    }
-
+    this.#tripRoutePoints = [...this.#routePointsModel.routePoints];
+    this.#renderRoutesPointList();
   }
+
+  #renderRoutesPointList(){
+    render(this.#routePointListComponent, this.#routePointListContainer);
+
+    for (let i = 0; i < this.#tripRoutePoints.length; i++) {
+      const destination = this.#destinationsModel.getById(this.#tripRoutePoints[i]);
+      const offers = this.#offersModel.getById(this.#tripRoutePoints[i]);
+      const offersByType = this.#offersModel.getByType(this.#tripRoutePoints[i]);
+      this.#renderRoutePoint(this.#tripRoutePoints[i], destination, offers, offersByType);
+    }
+  }
+
+  #renderRoutePoint(routePoint, destination, offers, offersByType) {
+    const escKeyDownHandler = (evt) => {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        replaceFormToRoutePoint();
+        document.removeEventListener('keydown', escKeyDownHandler);
+      }
+    };
+    const routePointComponent = new RoutePointView({routePoint, destination, offers, onClick: () => {
+      replaceRoutePointToForm(); document.addEventListener('keydown', escKeyDownHandler);
+    } });
+    const editFormViewComponent = new EditFormView({destination, routePoint, offers: offersByType, onFormSubmit: () => {
+      replaceFormToRoutePoint(); document.addEventListener('keydown', escKeyDownHandler);
+    } });
+    render(routePointComponent, this.#routePointListComponent.element);
+
+    function replaceRoutePointToForm () {
+      replace(editFormViewComponent, routePointComponent);
+    }
+    function replaceFormToRoutePoint(){
+      replace(routePointComponent, editFormViewComponent);
+    }
+  }
+
 }
